be16b45a6fc6a307310d65efd45d31d06e437665
elenawq <60143171+elenawq@users.noreply.github.com>
2020-06-16 15:45:34 +0300

    Updates

---
+++ precache-manifest.abd5c256cd1e6fd350e156c0b51e603c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7980d504046f464c3e746a8208d9d279",
+    "url": "/meditation-app/index.html"
+  },
+  {
+    "revision": "cfc7bca6a59d8ecfb9dd",
+    "url": "/meditation-app/static/css/main.e580daae.chunk.css"
+  },
+  {
+    "revision": "01338289bf14683eb5f9",
+    "url": "/meditation-app/static/js/2.3a34e5b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meditation-app/static/js/2.3a34e5b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc7bca6a59d8ecfb9dd",
+    "url": "/meditation-app/static/js/main.af08ca42.chunk.js"
+  },
+  {
+    "revision": "5a7f42c1e2012835d4ae",
+    "url": "/meditation-app/static/js/runtime-main.2d676833.js"
+  },
+  {
+    "revision": "177fcbafd7faeec4c23b24fcd4d48d62",
+    "url": "/meditation-app/static/media/arrow_button.177fcbaf.png"
+  },
+  {
+    "revision": "bd057b9d47a75514ffb41f4d96b5de15",
+    "url": "/meditation-app/static/media/background.bd057b9d.png"
+  },
+  {
+    "revision": "4bd3252c98b668305679a7a5d5fe60cd",
+    "url": "/meditation-app/static/media/background2.4bd3252c.png"
+  },
+  {
+    "revision": "cc76b3d5c49f2279dff89793341e1665",
+    "url": "/meditation-app/static/media/background3.cc76b3d5.png"
+  },
+  {
+    "revision": "c77444d9c15f1c90651add6e4b84b5f0",
+    "url": "/meditation-app/static/media/play.c77444d9.svg"
+  },
+  {
+    "revision": "58eedc543a110a95d7e968ab43adfcc0",
+    "url": "/meditation-app/static/media/title.58eedc54.png"
+  },
+  {
+    "revision": "e8719865089473acf3697ec74fc09b0e",
+    "url": "/meditation-app/static/media/woodland.e8719865.png"
+  }
+]);
